e7553676b4c25d21b232befe97f5d78e6450ba4c
Andrzej Ruszczewski <andrzej.ruszczewski@gmail.com>
2024-09-14 14:41:34 +0200

    exception handling

--- main.py
+++ main.py
@@ -9,19 +9,20 @@ option = st.selectbox("Select data to view", ('Temperature', 'Sky'))
 st.subheader(f"{option} for the next {days} days in {place}")
 
 if place:
-    dates, weather = backend.get_weather_data(place, days, option)
+    try:
+        dates, weather = backend.get_weather_data(place, days, option)
 
-    if not dates and not weather:
-        st.error("Please enter a valid place")
+        if option == 'Temperature':
+            labels = {'x': 'Date', 'y': 'Temperature'}
+            figure = px.line(x=dates, y=weather, labels=labels)
+            st.plotly_chart(figure)
 
-    if option == 'Temperature' and dates and weather:
-        labels = {'x': 'Date', 'y': 'Temperature'}
-        figure = px.line(x=dates, y=weather, labels=labels)
-        st.plotly_chart(figure)
+        if option == 'Sky':
+            image_paths = {'Clear': 'images/clear.png', 'Clouds': 'images/cloud.png', 'Rain': 'images/rain.png',
+                           'Snow': 'images/snow.png'}
+            for d in range(0, days):
+                st.image([image_paths[condition] for condition in weather[d*8:(d+1)*8]], width=85)
+                st.text(f"From {dates[d*8]} to {dates[(d+1)*7]}")
 
-    if option == 'Sky' and dates and weather:
-        image_paths = {'Clear': 'images/clear.png', 'Clouds': 'images/cloud.png', 'Rain': 'images/rain.png',
-                       'Snow': 'images/snow.png'}
-        for d in range(0, days):
-            st.image([image_paths[condition] for condition in weather[d*8:(d+1)*8]], width=85)
-            st.text(f"From {dates[d*8]} to {dates[(d+1)*7]}")
+    except ValueError:
+        pass
